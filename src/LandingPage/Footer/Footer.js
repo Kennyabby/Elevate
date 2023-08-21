5fc9952df34118f9a5fc60ae8a03afd98ae5e625
kennyabby <kennyabby22@gmail.com>
2023-08-21 12:42:50 +0100

    completed footer section

--- src/LandingPage/Footer/Footer.js
+++ src/LandingPage/Footer/Footer.js
@@ -1,10 +1,49 @@
 import './Footer.css'
 
 import { AiOutlineLinkedin } from 'react-icons/ai'
-import { FaInstagram } from 'react-icons/fa'
+import { FaInstagram, FaTwitter } from 'react-icons/fa'
 
 const Footer = () => {
-  return <></>
+  return (
+    <>
+      <footer className='ftcnt'>
+        <div className='ft'>
+          <div className='ftval'>
+            <div className='ftbld'>Washington</div>
+            <div className='ftlgt'>
+              {'4517 Washington Ave, Manchester, Kentucky 39495 (239) 555-0108'}
+            </div>
+          </div>
+          <div className='ftval'>
+            <div className='ftbld'>Paris</div>
+            <div className='ftlgt'>
+              {'4517 Washington Ave, Manchester, Kentucky 39495 (239) 555-0108'}
+            </div>
+          </div>
+          <div className='ftval'>
+            <div className='ftbld'>Hong Kong</div>
+            <div className='ftlgt'>
+              {'4517 Washington Ave, Manchester, Kentucky 39495 (239) 555-0108'}
+            </div>
+          </div>
+          <div className='ftval'>
+            <div className='ftbld'>London</div>
+            <div className='ftlgt'>
+              {'4517 Washington Ave, Manchester, Kentucky 39495 (239) 555-0108'}
+            </div>
+          </div>
+        </div>
+        <div className='ftbtm'>
+          <div className='ftscm'>
+            <AiOutlineLinkedin className='ftgap' />
+            <FaInstagram className='ftgap' />
+            <FaTwitter className='ftgap' />
+          </div>
+          <div className='ftcpr'>Copyright Figmaland 2020</div>
+        </div>
+      </footer>
+    </>
+  )
 }
 
 export default Footer
